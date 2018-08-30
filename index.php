f03cf36059ad096a3a097d838f486a431203e426
Mobarak Ali <mobarak.ali@gmail.com>
2018-08-30 23:07:48 +0600

    New File: functions.php Updated: index.php

--- index.php
+++ index.php
@@ -1,15 +1,15 @@
 <!doctype html>
-<html lang="en">
+<html lang="<?php bloginfo('language') ?>">
 <head>
-	<meta charset="UTF-8">
+	<meta charset="<?php bloginfo('charset'); ?>">
 	<meta name="viewport"
 	      content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<title>Document</title>
+	<title><?php bloginfo('name');  ?></title>
 </head>
 <body>
 
-    <p>This is the beginning! but right now it will not be able to work dynamically!</p>
+    <?php  bloginfo(name); ?>
 
 </body>
 </html>
